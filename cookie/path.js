749bba24891ab8ca0297b9c13204c5a1621e8c70
继风 <jifeng.zjd@taobao.com>
2013-02-19 19:52:56 +0800

    change cookie every time

--- cookie/path.js
+++ cookie/path.js
@@ -7,6 +7,7 @@
 var connect = require('connect');
 
 var app = connect();
+var count = 0;
 app.use('/test/index', function (req, res, next) {
   res.setHeader('Set-Cookie', 'cs=testindex; Max-Age=6307200000000; Path=/; Expires=Sat, 02 Jan 2213 09:34:50 GMT')
   // res.setHeader('Set-Cookie', 'cs=testindex; Max-Age=6307200000000; Expires=Sat, 02 Jan 2213 09:34:50 GMT')
@@ -15,7 +16,7 @@ app.use('/test/index', function (req, res, next) {
 });
 app.use('/index', function (req, res, next) {
   res.statusCode = 200;
-  res.setHeader('Set-Cookie', 'cs=mainindex; Max-Age=6307200000000; Path=/; Expires=Sat, 02 Jan 2213 09:34:50 GMT')
+  res.setHeader('Set-Cookie', 'cs=mainindex' + count++ + '; Max-Age=6307200000000; Path=/; Expires=Sat, 02 Jan 2213 09:34:50 GMT')
   res.end('main page');
 });
 
