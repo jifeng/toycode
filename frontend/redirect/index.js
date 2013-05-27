b10f94b0fa66278669bda5fb38de190fc074b680
继风 <jifeng.zjd@taobao.com>
2013-05-27 19:24:10 +0800

    http redirect

---
+++ frontend/redirect/index.js
@@ -0,0 +1,12 @@
+var connect = require('connect');
+
+var app = connect();
+app.use('/public', connect.static(__dirname + '/public'));
+app.use('/redirect/', function (req, res, next) {
+  res.statusCode = 302;
+  res.setHeader('Location', 'http://www.taobao.com');
+  res.end();
+});
+app.listen(1723, function () {
+  console.log('server is listening on port of 1723');
+});
