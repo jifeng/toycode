e539343c43d8fbf8bcb9b152850fe4930002837e
继风 <jifeng.zjd@taobao.com>
2013-08-07 20:11:58 +0800

    process

--- process/child.js
+++ process/child.js
@@ -1,6 +1,13 @@
+
+var optStr = process.env.options;
+
+console.log(typeof optStr, optStr);
+
 process.on('message', function(m) {
   console.log('CHILD got message:', m);
 });
 process.send({ foo: 'bar' });
 
-# console.log 123
+setInterval(function () {
+	console.log('hello world');
+}, 2000)
--- process/master.js
+++ process/master.js
@@ -2,9 +2,33 @@ var child_process = require('child_process');
 var path = require('path');
 
 var n = child_process.fork('./child.js', [], {
-  execPath: '/Users/jifeng/tools/nvm/v0.10.4/bin/node'
+  execPath: '/Users/jifeng/tools/nvm/v0.10.4/bin/node',
+  env: undefined
 });
 n.on('message', function(m) {
   console.log('PARENT got message:', m);
 });
 n.send({ hello: 'world' });
+
+
+process.on('SIGINT', function (){
+	console.log('SIGINT');
+});
+
+process.on('SIGQUIT', function (){
+	console.log('SIGQUIT');
+});
+
+process.on('exit', function (){
+	console.log('exit');
+});
+
+process.on('SIGTERM', function (){
+	console.log(arguments);
+	n.kill();
+	console.log('SIGTERM');
+});
+
+process.on('SIGUSR1', function (){
+	console.log('SIGUSR1');
+});
